aa0c61a049e07c44fa621505044ec577a77e7ba3
CHA-OMEN\caris <chahyun.dev@gmail.com>
2024-10-16 22:30:22 +0900

    [Feat] Building, Weapon CSV 생성, Resource의 Data 디렉토리 생성, Building, Resource 스크립터블 오브젝트 생성, CSVtoSO 클래스, 에디터 기능 개발

---
+++ Assets/01.Scripts/Cha/BuildingTech/BuildingData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildingType
+{
+    PowerPlant, // 발전실
+    BioFarm, // 바이오팜(농장)
+    Quarters, // 숙소
+    FuelPlant, // 연료실
+    ResearchLab, // 연구실
+    RecoveryRoom, // 회복실
+    RecreationRoom, // 오락실
+    SpecialFacility
+}
+
+[CreateAssetMenu(fileName = "BuildingData", menuName = "Building/Building Data")]
+public class BuildingData : ScriptableObject
+{
+    public int buildingId; 
+    public string name;
+    public BuildingType type;
+    public int baseCostEnergy; // 에너지 기본 비용
+    public int baseCostFood;
+    public int baseCostFuel;
+    public int baseCostWorkforce;
+    public float upgradeMultiplier; // 업그레이드 시 적용할 배율
+    public int maxLevel; // 업그레이드 가능 최대 레벨
+    public int productionOutput; // 생산 자원량 (기본 레벨 기준, 업그레이드 시 배율 적용)
+    public string description; // 빌딩 설명
+    public string prefabPath;
+}
